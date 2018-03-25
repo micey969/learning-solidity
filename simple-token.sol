a90139b93ed7ca7a66a9f4bccc61f971bc2a3494
micey969 <micey_969@hotmail.com>
2018-03-25 00:43:52 -0400

    feat(contract):added an update and cashout function

--- simple-token.sol
+++ simple-token.sol
@@ -1,87 +1,146 @@
 pragma solidity "0.4.21";
 
 contract SimpleToken {
-    /* This creates an array with all balances */
+
+    // ------------------------------------------------------------------------
+    // This creates an array with all balances
+    // ------------------------------------------------------------------------  
     mapping (address => uint256) public balances;
     mapping (address => uint256) public deposits;
     
+
+    // ------------------------------------------------------------------------
+    // This creates variables to hold
+    //  -Owner of the contract address
+    //  -Initial cost for a token
+    // ------------------------------------------------------------------------
     address owner;
-    address buyer;
-    uint256 cost; //the initial price for a token
-    uint256 _money; //the value the buyer enters
-    uint256 numTokens; //the number of tokens they can get
-    uint256 change = 0;  //refund incase they pay extra
-    
+    uint256 cost; 
 
-    /* Initializes contract with initial supply tokens to the creator of the contract */
+
+    // ------------------------------------------------------------------------
+    // Initialisation
+    //  -The contract token supply
+    //  -The owner of the contract  
+    //  -The token price
+    // ------------------------------------------------------------------------
     function SimpleToken(
-        uint256 initialSupply
+        uint256 initialSupply,
+        uint256 initialCost
         ) public payable {
-        balances[this] = initialSupply;     // Give the creator all initial tokens
+        balances[this] = initialSupply;     
         owner = msg.sender;
-        cost = msg.value;                  //Initializes the token price
+        cost = initialCost;                  
     }
 
-    /* Send coins */
-    function transfer(address _to) public {
-        numTokens = calculate();
-        require(_to != 0x0);        // Prevent transfer to 0x0 address.
-        require(balances[this] >= numTokens);           // Check if the sender has enough
-        require(balances[_to] + numTokens >= balances[_to]);  // Check for overflows
-        balances[this] -= numTokens;                    // Subtract from the sender
-        balances[_to] += numTokens;                           // Add the same to the recipient
+
+    // ------------------------------------------------------------------------
+    // Sends Coins
+    //  -Gets the sender's balance from deposit
+    //  -Receives token amount from calculate
+    //  -Prevents transfer to 0x0 address
+    //  -Check if the contract has enough tokens
+    //  -Check for overflows
+    //  -Subtract token amount from contract
+    //  -Add token amount to recipient balance
+    //  -Update both account balances
+    // ------------------------------------------------------------------------
+    function transfer(address _to) public{
+        uint256 numTokens = calculate();
+        require(_to != 0x0);        
+        require(balances[this] >= numTokens);           
+        require(balances[_to] + numTokens >= balances[_to]);  
+        balances[this] -= numTokens;                    
+        balances[_to] += numTokens;
+        update(numTokens);                           
     }
      
-     /*gets the amount of tokens */
-    function calculate() public returns (uint) {
-        uint tokens;
-        require(_money >= cost);   // Check if the sender has enough to buy
-        tokens = _money / cost; 
-        if (tokens > 5){   //limit buyers to 5 tokens maximum in one sale
-            tokens = 5;
+
+    // ------------------------------------------------------------------------
+    // Gets the amount of tokens
+    //  -Checks if there is enough money to buy
+    //  -Puts a limit on tokens bought in one transaction
+    //  -Calculates change if there is an an excess
+    //  -Double token value after each tranaction
+    //  -Returns the amount of token to transfer
+    // ------------------------------------------------------------------------
+    function calculate() public returns (uint256) {
+        uint256 numTokens;
+        require(deposits[msg.sender] >= cost);   
+        numTokens = deposits[msg.sender] / cost; 
+        if (numTokens > 5){   
+            numTokens = 5;
         }
-        change = _money - (cost * numTokens);
-        update (); //update both owner and clients balances
-        cost *= 2; //Currently doubles after each transfer-- possibly another way???
-        return tokens;
+        cost *= 2; 
+        return numTokens;
     }
      
-   function update() public {
-        uint256 price = _money - change;
-        
-        //gives an error
-        /*if(price == 0) throw;
-        if(!this.send(price)) throw;
-        return true;*/
 
-        //gives an error about lvalue
-        buyer.balance -= price;
-        this.balance += price;
-        deposits[buyer] = 0;
+    // ------------------------------------------------------------------------
+    // Update balances
+    //  -Get Ether amount
+    //  -Transfer Ether amount to owner
+    //  -Any remaining deposited amount stays in deposit
+    // ------------------------------------------------------------------------
+    function update(uint256 numTokens) public returns (bool success) {
+        uint256 price = (cost * numTokens);
+        deposits[msg.sender] -= price;
+        owner.transfer(price);
+        return true;
     }   
     
+
+    // ------------------------------------------------------------------------
+    // Deposit Ether
+    //  -Deposits input amount
+    //  -Allows for multiple deposit
+    // ------------------------------------------------------------------------
     function deposit() public payable {
-        buyer = msg.sender;
-        _money = msg.value;
-        deposits[buyer] += _money;    //in case they enter multiple times
+        deposits[msg.sender] += msg.value;    
     }
     
+
+    // ------------------------------------------------------------------------
+    // Transfer ownership of contract
+    //  -Verify the owner of the contract
+    //  -Switch owner to new address
+    // ------------------------------------------------------------------------
     function transferContractOwnership(address _to) public {
         require(msg.sender == owner);
         owner = _to;
     }
 
+    // ------------------------------------------------------------------------
+    // Destroys the contract
+    //  -Verify the owner of the contract
+    //  -Transfer contract funds to owner
+    //  -Destroy contract
+    // ------------------------------------------------------------------------
     function kill() public {
         require(msg.sender == owner);
-        selfdestruct(owner); //sends contract funds back to owner
+        selfdestruct(owner); 
     }
     
-    function ethBalance(address _user) public view returns (uint) {
+    // ------------------------------------------------------------------------
+    // Allow user to check their Ether balance
+    // ------------------------------------------------------------------------
+    function ethBalance(address _user) public view returns (uint256) {
         return _user.balance;   
     }
 
-    function TokenValue() public view returns (uint) {
-      return cost;   //check the cost of the token
+    // ------------------------------------------------------------------------
+    // Allow user to check the cost of a token
+    // ------------------------------------------------------------------------
+    function TokenValue() public view returns (uint256) {
+        return cost;   
+    }
+
+    // ------------------------------------------------------------------------
+    // Allow user to empty their deposit
+    // ------------------------------------------------------------------------
+    function Cashout(address _account) public {
+        _account.transfer(deposits[_account]);
+        deposits[_account] = 0;     
     }
 
 }
